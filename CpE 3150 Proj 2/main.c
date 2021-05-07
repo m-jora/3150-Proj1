b6125e9cd62aec49c480c33564056f485e51ea60
Harrison <hheselbarth@gmail.com>
2021-05-07 13:25:37 -0500

    nop's added

--- CpE 3150 Proj 2/main.c
+++ CpE 3150 Proj 2/main.c
@@ -11,38 +11,43 @@
  * increase brightness with each tick 
  * set initial brightness value
  * if timer not stopped before 2s strobe neopixels with short beep
- *
+ * Problem 5 from hw 5 for .1s delay with 2s total
  *
  *
  */ 
 
 #include <avr/io.h>
-#include "Adafruit_NeoPixel.h"
+#define _NOP() __asm__ __volatile__("nop")
+//#include "Adafruit_NeoPixel.h"
 
 void beep();
 
 void freq_delay();
 
-void setpixcolor(int n, int r, int g, int b);
+//void setpixcolor(int n, int r, int g, int b);
 
 int main(void)
 {
     while(1)
 	{
-		if(PIND & (1<<4)) // button get pressed
+		while ()
 		{
-			beep(); // beep time
-			while(PIND & (1<<4)); // waiting for button depress
-		}
 		
-		//beep(); makes the speaker go brrrr
+			if(PIND & (1<<4)) // button get pressed
+			{
+				beep(); // beep time
+				while(PIND & (1<<4)); // waiting for button depress
+			}
+		
+			//beep(); makes the speaker go brrrr
+		}
 	}
 }
 
-void setpixcolor(int n, int r, int g, int b)
+/*void setpixcolor(int n, int r, int g, int b)
 {
 	
-}
+}*/
 
 void beep() {
 	DDRC |= 0b01000000;
@@ -65,3 +70,4 @@ void freq_delay(){
 		TCCR0B = 0x00;
 		TIFR0 = 1<<TOV0;
 }
+

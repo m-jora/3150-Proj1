0cf6ebe5974bce35e7bbc3fb76553d72b3092e04
Harrison <hheselbarth@gmail.com>
2021-05-07 17:24:06 -0500

    all thats left is neopixels

--- CpE 3150 Proj 2/main.c
+++ CpE 3150 Proj 2/main.c
@@ -16,7 +16,15 @@
  *
  */ 
 
+// TCNT0 is 8 bit
+// TCNT1 & TCNT3 is 16bit
+// TCNT4 is 10 bit
+
+#define F_CPU 8000000UL
+
 #include <avr/io.h>
+#include <util/delay.h>
+
 #define _NOP() __asm__ __volatile__("nop")
 //#include "Adafruit_NeoPixel.h"
 
@@ -29,45 +37,61 @@ void freq_delay();
 int main(void)
 {
     while(1)
-	{
-		while ()
-		{
-		
-			if(PIND & (1<<4)) // button get pressed
-			{
-				beep(); // beep time
-				while(PIND & (1<<4)); // waiting for button depress
-			}
-		
-			//beep(); makes the speaker go brrrr
-		}
-	}
+    {
+        TCNT1H = -0xF4;
+        TCNT1L = 0x24;
+        TCCR1B = 0b00000100;
+        
+        while(!(TIFR1 & (1<<TOV1)))
+        {
+            TCNT3H = -0x30;
+            TCNT3L = 0xD4;
+            TCCR3B = 0b00000011;
+            while (!(TIFR3 & (1<<TOV3)))
+            {
+            
+                if(PIND & (1<<4)) // button get pressed
+                {
+                    int hold_low = TCNT1L;
+                    int hold_high = TCNT1H;
+                    beep(); // beep time
+                    while(PIND & (1<<4)); // waiting for button depress
+                    
+                    while (!(PIND & (1<<4)))
+                    {
+						TCNT1H = hold_high;
+						TCNT1L = hold_low;
+						TIFR1 = 1<<TOV1;
+                    }
+					beep();
+					while(PIND & (1<<4));
+                }
+        
+            }
+            TCCR3B = 0x00;
+            TIFR3 = 1<<TOV3;
+        }
+        TCCR1B = 0x00;
+        TIFR1 = 1<<TOV1;
+
+        beep(); // makes speaker go brrrrrrrrr
+    }
 }
 
 /*void setpixcolor(int n, int r, int g, int b)
 {
-	
+    
 }*/
 
 void beep() {
-	DDRC |= 0b01000000;
-	TCNT1H = -0x61;
-	TCNT1L = 0xBE;
-	TCCR1B = 0b00000011; 
-	while(!(TIFR1 & (1<<TOV1))){
-		freq_delay();
-		PORTC ^= 0b01000000;	
-	}
-	TCCR1B = 0x00;
-	TIFR1 = 1<<TOV1;
+    DDRC |= 0b01000000;
+    TCNT0 = -0xFF;
+    TCCR0B = 0b00000101;
+    
+    while(!(TIFR0 & (1<<TOV0))){
+        _delay_ms(1);
+        PORTC ^= 0b01000000;    
+    }
+    TCCR0B = 0x00;
+    TIFR0 = 1<<TOV0;
 }
-
-	
-void freq_delay(){
-		TCNT0 = -63;
-		TCCR0B = 0b00000011;
-		while(!(TIFR0 & (1<<TOV0)));
-		TCCR0B = 0x00;
-		TIFR0 = 1<<TOV0;
-}
-
